7ade5829aa3fdf154abcc6cbebe3e993533e137a
gfernandez09 <47225340+gfernandez09@users.noreply.github.com>
2021-05-20 16:19:19 +0200

    Code Commented

--- Usuari.py
+++ Usuari.py
@@ -1,14 +1,18 @@
 class Usuari:
+    # Constructor.
     def __init__(self, nom, credit):
         self.__nom = nom
         self.credit = credit
-
+    
+    # Método incrementar Credito.
     def incrementarCredit(self, quantitat):
         self.credit += quantitat
-
+    
+    # Método decrementar Credito.
     def decrementarCredit(self, quantitat):
         self.credit -= quantitat
-
+    
+    # Getters & Setters.
     def getNom(self):
         return self.__nom
 
@@ -17,6 +21,7 @@ class Usuari:
 
     def setCredit(self, credit):
         self.credit = credit
-
+    
+    # Str de la clase.
     def __str__(self):
         return str(self.__nom)
